2f94ac992052e8b5cffc121954575deb23a4b56c
Mathieu Clavier <mathieu.clavier@outlook.com>
2022-08-20 07:59:13 +0200

    update

--- tests/test_slapping.py
+++ tests/test_slapping.py
@@ -20,34 +20,34 @@ def test_single_slaps():
     ('lldd', LikeState.empty),
     ('ddl', LikeState.liked),
 ])
-# def test_multi_slaps(test_input, expected):
-#     assert slap_many(LikeState.empty, test_input) is expected
+def test_multi_slaps(test_input, expected):
+    assert slap_many(LikeState.empty, test_input) is expected
 
 
-# @pytest.mark.skip(reason="regexes not supported yet")
-# def test_regex_slaps():
-#     assert slap_many(LikeState.empty, '[ld]*ddl') is LikeState.liked
+@pytest.mark.skip(reason="regexes not supported yet")
+def test_regex_slaps():
+    assert slap_many(LikeState.empty, '[ld]*ddl') is LikeState.liked
 
 
-# @pytest.mark.xfail
-# def test_divide_by_zero():
-#     assert 1 / 0 == 1
+@pytest.mark.xfail
+def test_divide_by_zero():
+    assert 1 / 0 == 1
 
 
-# def test_invalid_slap():
-#     with pytest.raises(ValueError):
-#         slap_many(LikeState.empty, 'x')
+def test_invalid_slap():
+    with pytest.raises(ValueError):
+        slap_many(LikeState.empty, 'x')
 
 
-# @pytest.mark.xfail
-# def test_db_slap(db_conn):
-#     db_conn.read_slaps()
-#     assert ...
+@pytest.mark.xfail
+def test_db_slap(db_conn):
+    db_conn.read_slaps()
+    assert ...
 
 
-# def test_print(capture_stdout):
-#     print("hello")
-#     assert capture_stdout["stdout"] == "hello\n"
+def test_print(capture_stdout):
+    print("hello")
+    assert capture_stdout["stdout"] == "hello\n"
 
 # def test_many_slaps():
 #     assert slap_many(LikeState.empty, 'll') is LikeState.empty
