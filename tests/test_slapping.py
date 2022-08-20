0c883d26f70ef77b124ff1c09e1bfee243ad9609
Mathieu Clavier <mathieu.clavier@outlook.com>
2022-08-20 08:07:21 +0200

    update test

--- tests/test_slapping.py
+++ tests/test_slapping.py
@@ -2,13 +2,13 @@ import pytest
 from slapping.slap_that_like_button import LikeState, slap_many
 
 
-def test_empty_slap():
-    assert slap_many(LikeState.empty, '') is LikeState.empty
+# def test_empty_slap():
+#     assert slap_many(LikeState.empty, '') is LikeState.empty
 
 
-def test_single_slaps():
-    assert slap_many(LikeState.empty, 'l') is LikeState.liked
-    assert slap_many(LikeState.empty, 'd') is LikeState.disliked
+# def test_single_slaps():
+#     assert slap_many(LikeState.empty, 'l') is LikeState.liked
+#     assert slap_many(LikeState.empty, 'd') is LikeState.disliked
 
 
 @pytest.mark.parametrize("test_input,expected", [
@@ -24,30 +24,30 @@ def test_multi_slaps(test_input, expected):
     assert slap_many(LikeState.empty, test_input) is expected
 
 
-@pytest.mark.skip(reason="regexes not supported yet")
-def test_regex_slaps():
-    assert slap_many(LikeState.empty, '[ld]*ddl') is LikeState.liked
+# @pytest.mark.skip(reason="regexes not supported yet")
+# def test_regex_slaps():
+#     assert slap_many(LikeState.empty, '[ld]*ddl') is LikeState.liked
 
 
-@pytest.mark.xfail
-def test_divide_by_zero():
-    assert 1 / 0 == 1
+# @pytest.mark.xfail
+# def test_divide_by_zero():
+#     assert 1 / 0 == 1
 
 
-def test_invalid_slap():
-    with pytest.raises(ValueError):
-        slap_many(LikeState.empty, 'x')
+# def test_invalid_slap():
+#     with pytest.raises(ValueError):
+#         slap_many(LikeState.empty, 'x')
 
 
-@pytest.mark.xfail
-def test_db_slap(db_conn):
-    db_conn.read_slaps()
-    assert ...
+# @pytest.mark.xfail
+# def test_db_slap(db_conn):
+#     db_conn.read_slaps()
+#     assert ...
 
 
-def test_print(capture_stdout):
-    print("hello")
-    assert capture_stdout["stdout"] == "hello\n"
+# def test_print(capture_stdout):
+#     print("hello")
+#     assert capture_stdout["stdout"] == "hello\n"
 
 # def test_many_slaps():
 #     assert slap_many(LikeState.empty, 'll') is LikeState.empty
